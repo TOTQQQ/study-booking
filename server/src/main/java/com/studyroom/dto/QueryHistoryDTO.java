da3d3b3f47d885ab0c1bbe51dea170040c108266
qiuxun007 <3039253307@qq.com>
2026-05-29 11:24:12 +0800

    QueryHistoryDTO.java

---
+++ server/src/main/java/com/studyroom/dto/QueryHistoryDTO.java
@@ -0,0 +1,31 @@
+package com.studyroom.dto;
+
+public class QueryHistoryDTO {
+    private Long userId;
+    private Integer pageNum = 1;
+    private Integer pageSize = 10;
+    
+    public Long getUserId() {
+        return userId;
+    }
+    
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+    
+    public Integer getPageNum() {
+        return pageNum;
+    }
+    
+    public void setPageNum(Integer pageNum) {
+        this.pageNum = pageNum;
+    }
+    
+    public Integer getPageSize() {
+        return pageSize;
+    }
+    
+    public void setPageSize(Integer pageSize) {
+        this.pageSize = pageSize;
+    }
+}
